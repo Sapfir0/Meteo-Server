2c79e4ec8d7e4bc949f06f339d94116d704060fc
Sapfir0 <sapfir999999@yandex.ru>
2019-05-25 22:28:24 +0400

    хм

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("Произошел гет")
   res.render('index', { title: 'Express' });
 });
 
 router.post('/', (req,res,next) => {
+  console.log("Произошел пост")
   console.log(req.body)
   res.send(req.body)
 })
