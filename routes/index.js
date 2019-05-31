62262f9e758e8ef4b8e5b8b1f0d69d4f73ae7d34
Sapfir0 <sapfir999999@yandex.ru>
2019-06-01 00:44:19 +0400

    еще чутка

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express.Router();
 
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+//const bodyParser = require('body-parser');
+//const urlencodedParser = bodyParser.urlencoded({extended: false})
 
 const sendHtml = require("../services/sendHtml")
-const arduino = require("../services/handleArduinoDatas")
 const handler = require("../controllers/arduino")
 
 //-------- рендер страниц -------------
