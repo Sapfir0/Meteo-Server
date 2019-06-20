ff72038f3c38c261350fea12da30c092379b4477
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2019-06-20 11:59:13 +0400

    Images (#7)

---
+++ database/models/profiles.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, Sequelize) {
+    var Profile = sequelize.define('profiles', {
+        id: { 
+            autoIncrement: true, 
+            primaryKey: true, 
+            type: Sequelize.INTEGER 
+        },
+        oldWeatherDescriptionIcons: { 
+            type: Sequelize.BOOLEAN,
+            defaultValue: false 
+        },
+
+
+    });
+
+    return Profile;
+};
