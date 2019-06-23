04a27e20b5fe2d262c5bf1848d4ef926cf6c2cbd
Sapfir0 <sapfir999999@yandex.ru>
2019-06-23 15:36:31 +0400

    Просто фиксы

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
+describe('Test #1', function() {
+  describe('#1.1', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+      assert.equal([1, 2, 3, 4, 5, 67 ].indexOf(4), -1);
+
     });
   });
 });
